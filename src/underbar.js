b04c2fa88562aaf25c8909776558b5179b142af9
Si <siwang22@gmail.com>
2015-08-20 17:52:19 -0700

    Fixed _.each, allowing _.indexOf to work

--- src/underbar.js
+++ src/underbar.js
@@ -54,10 +54,10 @@
   _.each = function(collection, iterator) {
     if(Array.isArray(collection)) {
       for(var i = 0; i < collection.length; i++)
-        iterator(collection[i]);
+        iterator(collection[i],i,collection);
     } else {
       for(var k in collection)
-        iterator(collection[k]);
+        iterator(collection[k],k,collection);
     }
   };
 
