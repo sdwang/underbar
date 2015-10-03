a36faf4d8a0f1df7d62710eb42fc543b53bf28bc
Si <siwang22@gmail.com>
2015-10-03 11:59:06 -0700

    _.invoke works when provided with function reference

--- src/underbar.js
+++ src/underbar.js
@@ -434,6 +434,13 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    var results = [];
+
+    _.each(collection, function(value) {
+      results.push(functionOrKey.call(value));
+    });
+
+    return results;
   };
 
   // Sort the object's values by a criterion produced by an iterator.
